6246380d8b1769b6f4c0e093722bd32f01d78cdf
Maciek <maciek.pacut@gmail.com>
2011-11-07 13:07:08 +0100

    Fixed linker issues

--- src/permutation.cpp
+++ src/permutation.cpp
@@ -1,1 +1,7 @@
 #include "permutation.hpp"
+
+template<int N>
+  bool permutation<N>::valid_permutation() const
+{
+  return true;
+}
--- src/permutation.hpp
+++ src/permutation.hpp
@@ -3,11 +3,12 @@
 #include <array>
 #include <cassert>
 
+// permutations of N elements
 template<int N>
-class permutation
+  class permutation
 {
 private:
   std::array<int, N> perm;
+  bool valid_permutation() const;
 public:
-  
 };
