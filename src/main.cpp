c811d62222d8c7d8a7a4820f43fc1bb011a873f8
Maciek <maciek.pacut@gmail.com>
2011-11-07 13:47:48 +0100

    Random permutation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "random.hpp"
 #include "permutation.hpp"
 
 int main(int argc, char* argv[])
 {
-  permutation p(5);
+  init_random();
+  permutation p(5, permutation::type::random);
   std::cout << p << std::endl;
   return 0;
 }
