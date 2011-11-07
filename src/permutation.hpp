42733f8542ccf8258a8ba66c9f88d9322d4a2263
Maciek <maciek.pacut@gmail.com>
2011-11-07 13:26:43 +0100

    Explained representation of comments and fixed even more linker errors

--- src/permutation.hpp
+++ src/permutation.hpp
@@ -5,6 +5,19 @@
 
 #include "random.hpp"
 
+/*
+ * Representation of permutation:
+ * * vector of integers
+ * * elements of vector are in <0...N-1>
+ * * every element of vector is distinct
+ *
+ * example:
+ * * vector [ 1, 0, 3, 2 ]
+ * * represents algebraic permutation:
+ * * / 1 2 3 4 \
+ * * \ 2 1 4 3 /
+ */
+
 class permutation
 {
 private:
