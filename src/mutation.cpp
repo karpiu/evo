e15bc1dd3173d8aec7d4f9a808bf7ba0e1b176f2
Maciek <maciek.pacut@gmail.com>
2011-11-07 13:56:39 +0100

    Mutation operator

--- src/mutation.cpp
+++ src/mutation.cpp
@@ -1,1 +1,8 @@
 #include "mutation.hpp"
+
+void mutation::random_transposition(permutation& p)
+{
+  const int a = rand() % p.N();
+  const int b = rand() % p.N();
+  p.transpose(a, b);
+}
--- src/mutation.hpp
+++ src/mutation.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
 #include "permutation.hpp"
+
+namespace mutation
+{
+  void random_transposition(permutation& p);
+}
