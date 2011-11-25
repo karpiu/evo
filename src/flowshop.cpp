6613caa46f8f6c8a294bd5927c2c62fbd472bc5a
Michał Karpiński <karp0506@gmail.com>
2011-11-25 20:59:38 +0100

    memory leak in cmax fixed

--- src/flowshop.cpp
+++ src/flowshop.cpp
@@ -1,4 +1,3 @@
-
 #include "flowshop.hpp"
 
 flowshop::flowshop()
@@ -135,6 +134,8 @@ int flowshop::cmax(const std::vector<int> &v)
       C[j] = std::max(C[j-1], C_prev[j]) + machs[j].time_table[v[i]];
     std::swap( C, C_prev ); // swap pointers 
   }
+  delete C;
+  delete C_prev;
   
   return C_prev[size-1];
 }
