0806925290fd976b4d2f43b6810fff5751f44f5c
Michał Karpiński <karp0506@gmail.com>
2011-11-27 12:06:00 +0100

    typo fixed

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ po::options_description command_line_args_create()
     ("raport-every-frame,e", po::value<std::string>(), "Raports every frame. Available: best, avg.")
     ("max-iter,i", po::value<int>()->default_value(1000), "Maximum number of iterations.")
     ("crossover,x", po::value<std::string>()->default_value("pmx"), "Crossover operators. Available: pmx, cx, ox")
-    ("smart-termination,t", "Use termination condition that does not depends on --max-iter. Also overrides --max-iter")
+    ("smart-termination,t", "Use termination condition that does not depend on --max-iter. Also overrides --max-iter")
     ;
   return command_line_args;
 }
