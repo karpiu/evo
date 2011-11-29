b98996351a0435cbf0c56ca51d969d97f111b753
Michał Karpiński <karp0506@gmail.com>
2011-11-29 09:31:14 +0100

    avg_specimen and population variable added

--- src/statistics.cpp
+++ src/statistics.cpp
@@ -23,3 +23,40 @@ int statistics::edit_distance( const permutation& a, const permutation& b )
   
   return res;
 }
+
+int statistics::var( const population& p, const int i )
+{
+  int n = p.size();
+  int sum = 0;
+  int e;
+  
+  for(int j=0; j<n; j++)
+  {
+    if(i == j)
+      continue;
+    e = edit_distance(p[i].perm,p[j].perm);
+    sum += e*e;
+  }
+
+  return sum;
+}
+
+// returns index of the avarage specimen in population p
+int statistics::avg_specimen( const population& p)
+{
+  int avg = 0;
+  int v = var(p, avg);
+  int v2;
+
+  for(unsigned int i=1; i<p.size(); i++)
+  {
+    v2 = var(p, i);
+    if( v2 < v )
+    {
+      avg = i;
+      v = v2;
+    }
+  }
+  return avg;
+}
+
--- src/statistics.hpp
+++ src/statistics.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "permutation.hpp"
+#include "sga.hpp"
 
 namespace statistics
 {
   int edit_distance( const permutation& a, const permutation& b );
+  int var( const population& p, const int i);
+  int avg_specimen( const population& p);
 }
