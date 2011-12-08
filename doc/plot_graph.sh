88a988c6977d57c1ae3fd15c74ea47cbb2db1e68
Maciek <maciek.pacut@gmail.com>
2011-12-08 14:56:49 +0100

    Optimum is in optimum.dat while plotting

--- doc/plot_graph.sh
+++ doc/plot_graph.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cat $1 > graph.dat
 graphs=`./count_graphs.sh $1`
+./optimum.sh ${1%.res} > optimum.dat
 file="graph"$graphs".gnuplot"
 gnuplot $file > tmp.eps
 epstopdf tmp.eps --outfile=$2
-rm tmp.eps
-rm graph.dat
+rm -f optimum.dat
+rm -f tmp.eps
+rm -f graph.dat
