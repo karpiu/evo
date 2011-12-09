9229331363f7bc86cf6151e4b46c7f04f64de3e0
Michal Karpinski <karp0506@gmail.com>
2011-12-09 15:05:39 +0100

    scripts for crossover comparison

---
+++ doc/compareall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function zeros {
+	if [ $1 -lt 10 ] ; then
+		zret="00"	
+	else
+		if [ $1 -lt 100 ] ; then
+			zret="0"
+		else
+			zret=""
+		fi
+	fi
+}
+
+for i in {1..100}; do
+	zeros $i	
+	z=$zret
+	./comparex.sh $z$i 500
+done
