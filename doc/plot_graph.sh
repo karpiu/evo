108685e153cea832f2103811176dd0a67af04896
Michal Karpinski <karp0506@gmail.com>
2011-12-09 23:02:37 +0100

    separating drawing graphs with or without optimum line

--- doc/plot_graph.sh
+++ doc/plot_graph.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 cat $1 > graph.dat
 graphs=`./count_graphs.sh $1`
-
-if [ $graphs -gt 2 ]
-then
-	touch graphplot.tmp
-	file="graph"$graphs".gnuplot.template"
-	s="t"$1
-	opt=$(grep ${s:0:4} ../in/optimal-cmax | cut -f2)
-	echo "o="$opt >> graphplot.tmp
-	cat $file >> graphplot.tmp
-	gnuplot graphplot.tmp > tmp.eps
-	rm graphplot.tmp
-else
-	file="graph"$graphs".gnuplot"
-	gnuplot $file > tmp.eps
-fi
-
-
+file="graph"$graphs".gnuplot"
+gnuplot $file > tmp.eps
 epstopdf tmp.eps --outfile=$2
 rm tmp.eps
 rm graph.dat
