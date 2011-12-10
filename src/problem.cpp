001c76ba40952e3635ce6a549dd3e3094e333ccf
Michal Karpinski <karp0506@gmail.com>
2011-12-10 12:19:42 +0100

    operator statistics output better suited for plotting

--- src/problem.cpp
+++ src/problem.cpp
@@ -272,10 +272,7 @@ void report_end(population& p)
 
     if(cfg.compare_operators)
     {
-      std::cout << "ox = " << ox_count << "\n";
-      std::cout << "cx = " << cx_count << "\n";
-      std::cout << "pmx = " << pmx_count << "\n";
-      std::cout << "crossovers = " << x_count << "\n";
+      std::cout << ox_count << ' ' << cx_count << ' ' << pmx_count << ' ' << x_count << "\n";
     }
   }
 }
