af12c4928badabcb7a32100116984748b4eb3833
maciek <maciek@debian>
2011-12-11 10:01:40 +0100

    Non relaying on default population size in compareall_random test

--- doc/compare_random.sh
+++ doc/compare_random.sh
@@ -14,7 +14,7 @@ iter=$2
 echo "${num}ao.res: ../in/${num}.in" >> makefile.tmp
 echo "	../src/brute.e -e 100 -i ${iter}00 < $< > \$@" >> makefile.tmp
 echo "${num}bo.res: ../in/${num}.in" >> makefile.tmp
-echo "	../src/evo.e -e avg -i ${iter} -x pmx < $< > \$@" >> makefile.tmp
+echo "	../src/evo.e -e avg -i ${iter} -x pmx -p 100 < $< > \$@" >> makefile.tmp
 echo "${num}o.res: ${num}ao.res ${num}bo.res" >> makefile.tmp
 echo "	./joinN.sh ${num}ao.res ${num}bo.res > \$@" >> makefile.tmp
 echo "${num}o.pdf: ${num}o.res" >> makefile.tmp
